8f7729eb97a1111efd79ede399b259bed9e02c4f
dg288 <dg794619@gmail.com>
2025-06-27 15:58:43 +0200

    commit 1

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <containerfluid>
+      <head>
+        <meta charset="UTF-8" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous"></link>
+        <title><strong>Portfolio</strong></title>
+      </head>
+      <body>
+        <input type="text" placeholder='Enter your name'/>
+        
+      </body>
+
+  </containerfluid>
+  )
+}
+
+export default App
